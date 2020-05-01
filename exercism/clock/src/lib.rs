1bb57f9ac2c3149d0d4a548b6f7708df8f40ffb3
gcastellov <dreeyfus20@hotmail.com>
2020-05-01 21:30:48 +0200

    second approach for clock exercise

--- exercism/clock/src/lib.rs
+++ exercism/clock/src/lib.rs
@@ -13,22 +13,24 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
 
-        let mut _hours = hours;
-        let mut _minutes = minutes;
-        let mm = _minutes % MINUTES;
-        _hours += _minutes / MINUTES;
+        let mut _hours = match minutes.checked_div_euclid(MINUTES) {
+            Some(h) => hours + h,
+            _ => hours
+        };
+
+        let _minutes = match minutes.checked_rem_euclid(MINUTES) {
+            Some(m) => m,
+            _ => minutes
+        };
+
+        _hours = match _hours.checked_rem_euclid(HOURS) {
+            Some(h) => h,
+            _ => 0
+        };
         
-        if mm < 0 {
-            _hours -=1;
-        }
-
-        let hh = _hours % HOURS;
-        let get_hours = |hh| (if hh >= 0 { hh } else if hh < 0 {  HOURS + hh } else { _hours });
-        let get_minutes = |mm| (if mm >= 0 { mm } else if mm < 0 { MINUTES + mm } else { _minutes } );
-
         Clock { 
-            hours: get_hours(hh), 
-            minutes: get_minutes(mm)
+            hours: _hours, 
+            minutes: _minutes
         }
     }
 
@@ -36,14 +38,10 @@ impl Clock {
         let add_minutes: i32 = self.minutes + minutes;
         Self::new(self.hours, add_minutes)
     }
-
-    fn show(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
-    }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.show(f)
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
