8c3c5d03583da1eeb9573182b8f6e3263c1be608
gcastellov <dreeyfus20@hotmail.com>
2020-05-23 21:37:49 +0200

    parsing age as Result

--- db-in-memory/src/main.rs
+++ db-in-memory/src/main.rs
@@ -1,4 +1,5 @@
 mod management;
+use std::num::ParseIntError;
 use management::storage;
 use management::people::*;
 use std::io;
@@ -11,12 +12,17 @@ const EXIT_OPTION: &'static str = "E";
 
 trait OrElse {
     fn or_else(&self, _else: String) -> String;
+    fn is_empty_or_whitespace(&self) -> bool;
 }
 
 impl OrElse for String {    
     fn or_else(&self, _else: String) -> String { 
         let copy = &*self;
         if copy.is_empty() { _else } else { copy.to_string() }
+    }
+
+    fn is_empty_or_whitespace(&self) -> bool {
+        self.trim().is_empty() 
     }
 }
 
@@ -66,10 +72,25 @@ fn list<'a>(db: &'a mut storage::Database) {
 }
 
 fn create<'a>(db: &'a mut storage::Database) {
-    println!("Type the following to create new person:");
-    let personal_info = get_personal_info();
-    let person = Person::new(&personal_info.0, &personal_info.1, personal_info.2);
-    db.add(person);
+    let mut is_completed = false;
+    
+    while !is_completed {
+        println!("Type the following to create new person:");
+        let personal_info = get_personal_info();
+        is_completed = !personal_info.0.is_empty_or_whitespace() 
+            && !personal_info.1.is_empty_or_whitespace() 
+            && personal_info.2.is_ok();
+
+        if !is_completed {
+            println!("");
+            println!("Please check your inputs. All data are required!");
+            println!("");
+        }
+        else {
+            let person = Person::new(&personal_info.0, &personal_info.1, personal_info.2.unwrap());
+            db.add(person);
+        }
+    }
 }
 
 fn update<'a>(db: &'a mut storage::Database) {
@@ -110,10 +131,12 @@ fn update_person<'a>(db: &'a mut storage::Database) {
     let entry = find_by_id(db);
     let person: &Person = &*entry.1;
     let personal_info = get_personal_info();
+    let age = if personal_info.2.is_err() { person.age } else { personal_info.2.unwrap() };
+
     let mut copy = Person::new(
         &personal_info.0.or_else(person.name.to_owned()), 
         &personal_info.1.or_else(person.last_name.to_owned()), 
-        personal_info.2);
+        age);
 
     if let Some(address) = &person.address {
         copy.set_address(&address.city, &address.street, address.number, &address.zip_code);
@@ -122,7 +145,7 @@ fn update_person<'a>(db: &'a mut storage::Database) {
     db.people.insert(entry.0, copy);
 }
 
-fn get_personal_info() -> (String, String, u16) {
+fn get_personal_info() -> (String, String, Result<u16, ParseIntError>) {
     println!("Name:");
     let name = read_line();
     
@@ -131,7 +154,7 @@ fn get_personal_info() -> (String, String, u16) {
     
     println!("Age:");
     let _age = read_line();
-    let age = _age.parse::<u16>().unwrap();
+    let age = _age.parse::<u16>();
 
     (name, last_name, age)
 }
